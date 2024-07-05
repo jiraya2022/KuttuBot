6df30a436ee35c075e45212a3a365d7c1c6d7a60
THH <159997953+The-Happy-Hour@users.noreply.github.com>
2024-07-05 14:33:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/GouthamSER/KuttuBot.git /KuttuBot
+  git clone https://github.com/The-Happy-Hour/KuttuBot /KuttuBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /KuttuBot
