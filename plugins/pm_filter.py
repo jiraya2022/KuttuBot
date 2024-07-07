8c95aa3f647ca97f5cc313d8a1cbcb40dee62b82
THH <159997953+The-Happy-Hour@users.noreply.github.com>
2024-07-07 22:34:22 +0530

    auto delete addded

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -579,14 +579,14 @@ async def auto_filter(client, msg, spoll=False):
         try:
             jk=await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
                                       reply_markup=InlineKeyboardMarkup(btn))
-await asyncio.sleep(300)
-await jeetu.delete()
+            await asyncio.sleep(300)
+            await jk.delete()
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
             pic = imdb.get('poster')
             poster = pic.replace('.jpg', "._V1_UX360.jpg")
-            await message.reply_photo(photo=poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
-await asyncio.sleep(300)
-await jeetu.delete()
+            jk=await message.reply_photo(photo=poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
+            await asyncio.sleep(300)
+            await jk.delete()
         except Exception as e:
             logger.exception(e)
             await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
